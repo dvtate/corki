0c578b23cc60e3950ae3f4d7acf17a45bdd89344
Dustin Van Tate Testa <toast27@gmail.com>
2018-04-16 12:40:28 -0400

    change structure

--- corki.sh
+++ corki.sh
@@ -1,33 +1,10 @@
 #!/bin/sh
 
+# get key from file
+export DISC_KEY=$(cat $HOME/.corki/disc_key)
 
-## this script is used to run the bot
-## in addition it helps to maintain privacy of our bot token
+echo "starting corki bot..."
+# start bot
+node index.js
 
-
-export DISCORD_TOKEN=token goes here
-
-# gets run after Corki is killed
-function cleanup {
-	printf "Killing Corki... "
-	kill $CORKI_PID
-	printf "done\n"
-}
-
-# run cleanup before exiting
-trap cleanup EXIT
-
-# gets run before Corki is started
-printf "Spawning Corki... "
-
-# start Corki & mark his PID
-node index.js &
-CORKI_PID=$!
-
-printf "done\n"
-
-# wait for eternity... unless they kill me...
-cat
-
-# this should never get run
-echo "Corki -> error -> cat abuse..."
+echo "gg bot died :/"
