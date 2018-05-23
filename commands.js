b069db0f63b9a04e4ac8d1953cb7da0194c7a77e
Dustin Van Tate Testa <toast27@gmail.com>
2018-05-22 18:13:13 -0700

    style done, implementation comes next

---
+++ commands.js
@@ -0,0 +1,25 @@
+
+let commands = [
+
+    {
+        category: "general",
+        name: "-help",
+        usage: "<p>View a list of help commands to help you get started with the bot</p>",
+        args: "takes no arguments",
+        example: "<kbd>-help</kbd>"
+    }, {
+        category: "fun",
+        name: "-8ball",
+        usage: "<p>answers yes, no or maybe</p>",
+        args: "optional question<br/><kbd>-8ball [question]</kbd>",
+        example: "<kbd>-8ball Are yordles better than humans?</kbd>"
+    }, {
+        category: "general",
+        name: "-echo",
+        usage: "<p>repeats a given message</p>",
+        args: "message to repeat<br><kbd>-echo &lt;message></kbd>",
+        example: "<kbd>-echo repeat after me</kbd><br><kbd>-echo -echo -echo hehe x3</kbd>"
+    }
+
+
+]
