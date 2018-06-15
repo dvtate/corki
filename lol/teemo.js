1c3fba4cd1a40ff31947a0caeb247341fa62f15d
Dustin Van Tate Testa <toast27@gmail.com>
2018-06-14 21:44:38 -0400

    ru doesnt follow conventions

--- lol/teemo.js
+++ lol/teemo.js
@@ -52,7 +52,7 @@ module.exports.serverNames = {
     "br"  : "br1", "eune" : "eun1", "euw" : "euw1",
     "jp"  : "jp1", "kr"  : "kr",   "lan" : "la1",
     "las" : "la2",  "na" : "na1",  "oce" : "oc1",
-    "tr"  : "tr1", "ru"  : "ru1",  "pbe" : "pbe1"
+    "tr"  : "tr1", "ru"  : "ru",  "pbe" : "pbe1"
 };
 
 
